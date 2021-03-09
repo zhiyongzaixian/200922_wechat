5e33b7e5e7d09dc9e5b7586ef79b7b3ecafa1d1b
zhiyongzaixian <13852292775@163.com>
2021-03-09 11:56:29 +0800

    '0309'

---
+++ wechat_demo/app.js
@@ -0,0 +1,21 @@
+// app.js
+
+// 注册小程序应用，生成小程序应用实例
+App({
+  onLaunch() {
+    // 展示本地存储能力
+    const logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+  },
+  globalData: {
+    userInfo: null
+  }
+})
